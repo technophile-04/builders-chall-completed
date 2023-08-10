1f63fa41bf8b9e7d606514fc1998d8dd9167742d
Mertcan <59885513+mertcanciy@users.noreply.github.com>
2023-08-10 23:13:44 +0300

    Excel export, other challenges were added (#1)
    Co-authored-by: Shiv Bhonde <shivbhonde04@gmail.com>

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,12 +1,20 @@
 import { useEffect, useState } from "react";
 import type { NextPage } from "next";
 import CopyToClipboard from "react-copy-to-clipboard";
+import * as XLSX from "xlsx";
 import { CheckCircleIcon, DocumentDuplicateIcon } from "@heroicons/react/24/outline";
 import { MetaHeader } from "~~/components/MetaHeader";
 import { Spinner } from "~~/components/Spinner";
 import { displayTxResult } from "~~/components/scaffold-eth";
 
-const challenges_id = ["decentralized-staking", "dice-game", "simple-nft-example", "token-vendor"] as const;
+const challenges_id = [
+  "decentralized-staking",
+  "dice-game",
+  "simple-nft-example",
+  "token-vendor",
+  "minimum-viable-exchange",
+  "state-channels",
+] as const;
 
 type CHALLENGE_DATA = {
   autoGrading: boolean;
@@ -40,16 +48,30 @@ const Home: NextPage = () => {
       setLoading(true);
       const res = await fetch(API_URL);
       const data = (await res.json()) as BUILDER_DATA[];
-      const buildersWithStakingChallengeAccepted = data.filter(
+      console.log("DATA", data);
+      const builderWithSelectedChallengeAccepted = data.filter(
         builder => builder.challenges?.[selectedChallengeId]?.status === "ACCEPTED",
       );
       // setBuilderWithStakingChallengeAccepted(buildersWithStakingChallengeAccepted);
-      setAcceptedBuildersAdderess(buildersWithStakingChallengeAccepted.map(builder => builder.id));
+      setAcceptedBuildersAdderess(builderWithSelectedChallengeAccepted.map(builder => builder.id));
       setLoading(false);
     };
 
     fetchData();
   }, [selectedChallengeId]);
+
+  const handleDownloadExcel = () => {
+    const dataToExport = acceptedBuilderAddress.map((address, index) => ({
+      BuilderIndex: index + 1,
+      BuilderAddress: address,
+    }));
+
+    const ws = XLSX.utils.json_to_sheet(dataToExport);
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "Builders");
+
+    XLSX.writeFile(wb, `${selectedChallengeId}_challenge_accepted_builders.xlsx`);
+  };
 
   return (
     <>
@@ -58,20 +80,13 @@ const Home: NextPage = () => {
         <div className="px-5">
           <h1 className="text-center mb-8">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl font-bold">ACCEPTED BUIDLERS</span>
           </h1>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold">packages/nextjs/pages/index.tsx</code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract <code className="italic bg-base-300 text-base font-bold">YourContract.sol</code> in{" "}
-            <code className="italic bg-base-300 text-base font-bold">packages/hardhat/contracts</code>
-          </p>
+          <p className="text-center text-lg">Choose the challenge you want to see!</p>
         </div>
         {/*Use a daisUI select to render all challenges_id*/}
         <select
-          className="select select-bordered w-64 mb-8"
+          className="select select-bordered w-64"
           value={selectedChallengeId}
           onChange={e => setSelectedChallengeId(e.target.value as (typeof challenges_id)[number])}
         >
@@ -82,40 +97,21 @@ const Home: NextPage = () => {
           ))}
         </select>
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
+        <div className="flex-grow w-full mt-4 px-8 py-2">
           {/*Render Builders list*/}
-          <div className="flex flex-col items-center justify-center">
-            <h2 className="text-center text-2xl mb-8">Builders {acceptedBuilderAddress.length}</h2>
+          <div className="flex flex-col items-center justify-center gap-4">
+            <h2 className="text-center text-2xl m-0">Builders {acceptedBuilderAddress.length}</h2>
             {loading ? (
-              <Spinner width="75" height="75" />
+              <Spinner width="55" height="55" />
             ) : (
-              <div className="grid w-3/4">
-                {/* {builderWithStakingChallengeAccepted.map(builder => ( */}
-                {/*   <div key={builder.id} className="flex flex-col bg-base-100 rounded-lg p-6 hover:bg-base-200"> */}
-                {/*     <div className="flex items-center justify-center mb-4"> */}
-                {/*       <SparklesIcon className="w-10 h-10 text-yellow-500" /> */}
-                {/*     </div> */}
-                {/*     <a */}
-                {/*       href={`https://app.buidlguidl.com/builders/${builder.id}`} */}
-                {/*       target="_blank" */}
-                {/*       className="text-xl font-bold text-center mb-4" */}
-                {/*       rel="noreferrer" */}
-                {/*     > */}
-                {/*       {builder.id} */}
-                {/*     </a> */}
-                {/*     <div className="flex items-center justify-center"> */}
-                {/*       <BugAntIcon className="w-6 h-6 text-red-500" /> */}
-                {/*       <span className="ml-2">{builder.challenges?.["decentralized-staking"]?.status}</span> */}
-                {/*     </div> */}
-                {/*   </div> */}
-                {/* ))} */}
-                <div>
-                  {addressCopied ? (
-                    <CheckCircleIcon
-                      className="ml-1.5 text-xl font-normal text-sky-600 h-12 w-12 cursor-pointer"
-                      aria-hidden="true"
-                    />
-                  ) : (
+              <div className="grid w-3/4 gap-4">
+                <div className="flex flex-col items-center">
+                  <div className="flex gap-2">
+                    <div className="">
+                      <button className="btn btn-primary btn-sm" onClick={handleDownloadExcel}>
+                        Download Excel
+                      </button>
+                    </div>
                     <CopyToClipboard
                       text={JSON.stringify(acceptedBuilderAddress)}
                       onCopy={() => {
@@ -125,15 +121,22 @@ const Home: NextPage = () => {
                         }, 800);
                       }}
                     >
-                      <DocumentDuplicateIcon
-                        className="ml-1.5 text-xl font-normal text-sky-600 h-12 w-12 cursor-pointer"
-                        aria-hidden="true"
-                      />
+                      {addressCopied ? (
+                        <CheckCircleIcon
+                          className="ml-1.5 text-xl font-normal text-sky-600 h-8 w-8 cursor-pointer"
+                          aria-hidden="true"
+                        />
+                      ) : (
+                        <DocumentDuplicateIcon
+                          className="ml-1.5 text-xl font-normal text-sky-600 h-8 w-8 cursor-pointer"
+                          aria-hidden="true"
+                        />
+                      )}
                     </CopyToClipboard>
-                  )}
-                  <div className="overflow-auto bg-secondary rounded-t-none rounded-3xl">
-                    <pre className="text-xs pt-4">{displayTxResult(acceptedBuilderAddress)}</pre>
                   </div>
+                </div>
+                <div className="overflow-auto bg-secondary rounded-3xl flex items-center">
+                  <pre className="text-xs p-4">{displayTxResult(acceptedBuilderAddress)}</pre>
                 </div>
               </div>
             )}
